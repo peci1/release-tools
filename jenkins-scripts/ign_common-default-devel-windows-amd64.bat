3c382e62eea2e998036b18ea5bf39978f8ca63ed
Jose Luis Rivero <jrivero@osrfoundation.org>
2020-10-27 11:53:01 +0100

    Try to use colcon + vcpkg

--- jenkins-scripts/ign_common-default-devel-windows-amd64.bat
+++ jenkins-scripts/ign_common-default-devel-windows-amd64.bat
@@ -4,6 +4,9 @@ set VCS_DIRECTORY=ign-common
 set PLATFORM_TO_BUILD=x86_amd64
 set IGN_CLEAN_WORKSPACE=true
 
-set DEPEN_PKGS="ffmpeg"
+set DEPEN_PKGS="dlfcn-win32 ffmpeg"
 
-call "%SCRIPT_DIR%/lib/generic-default-devel-windows.bat"
+set COLCON_PACKAGE=ignition-gui
+set COLCON_AUTO_MAJOR_VERSION=true
+
+call "%SCRIPT_DIR%\lib\colcon-default-devel-windows.bat"
