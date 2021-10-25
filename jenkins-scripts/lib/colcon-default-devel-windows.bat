bb7b5bc3863fed1dc7fe2a884bbb41e3198f7726
Jose Luis Rivero <jrivero@osrfoundation.org>
2021-10-25 18:23:25 +0200

    Debug errorcode

--- jenkins-scripts/lib/colcon-default-devel-windows.bat
+++ jenkins-scripts/lib/colcon-default-devel-windows.bat
@@ -105,7 +105,9 @@ if exist %LOCAL_WS_SOFTWARE_DIR%\configure.bat (
 
 echo # BEGIN SECTION: compiling %VCS_DIRECTORY%
 cd %LOCAL_WS%
-call %win_lib% :build_workspace %COLCON_PACKAGE% || goto :error
+echo "ERRORLEVEL BEFORE IS %errorlevel%"
+call %win_lib% :build_workspace %COLCON_PACKAGE%
+echo "ERRORLEVEL AFTER IS %errorlevel%"
 echo # END SECTION
 
 if "%ENABLE_TESTS%" == "TRUE" (
